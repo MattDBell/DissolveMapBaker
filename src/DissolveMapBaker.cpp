1df0c020fe527b9b0c6390d551e676ea4c41b0f4
Matthew Bell <MattDavidBell@gmail.com>
2016-05-20 14:56:31 -0700

    [Other] Added in more messaging [Fix] Copy from mapped texture to bmp correctly

--- src/DissolveMapBaker.cpp
+++ src/DissolveMapBaker.cpp
@@ -291,37 +291,85 @@ bool DissolveMapBaker::RunOnFolder(string folderName, string outputFile)
 	pImmediate->CSSetShaderResources(0, 1, &pTexArrayResourceView);
 	pImmediate->CSSetUnorderedAccessViews(0, 1, &pTexOutputView, NULL);
 
+	printf("Displatching shader\n");
 	pImmediate->Dispatch(width, height, 1);
 	pImmediate->CSSetShader(nullptr, nullptr, 0);
 
 	// Have to copy from our output to our staging then we map the staging
+	printf("Copying output to staging texture\n");
 	pImmediate->CopyResource(pTexStaging, pTexOutput);
 	D3D11_MAPPED_SUBRESOURCE mapped;
 	result = pImmediate->Map(pTexStaging, 0, D3D11_MAP_READ, 0, &mapped);
+	if(FAILED(result))
+	{
+		printf("Failed to Map Texture %x\n", result);
+		return false;
+	}
+
+	printf("Mapped Texture - RowPitch: %u DepthPitch: %u TexWidth: %u TexHeight: %u\n", mapped.RowPitch,
+		mapped.DepthPitch, stagingDesc.Width, stagingDesc.Height);
 
 	// Create the Bitmap, copy data to it then save it out
 	IWICBitmap* pToPng;
 	WICRect all{ 0, 0, stagingDesc.Width, stagingDesc.Height };
 	IWICBitmapLock* pLock;
-	pFactory->CreateBitmap(stagingDesc.Width, stagingDesc.Height,
+	result = pFactory->CreateBitmap(stagingDesc.Width, stagingDesc.Height,
 		GUID_WICPixelFormat8bppGray, WICBitmapCacheOnLoad, &pToPng);
+
+	if (FAILED(result))
+	{
+		printf("Failed to create Output Bitmap %x\n", result);
+		return false;
+	}
 
 	// Lock to write
 	result = pToPng->Lock(&all, WICBitmapLockWrite, &pLock);
 
-	UINT size;
-	WICInProcPointer* pData = new WICInProcPointer;
-
-	pLock->GetDataPointer(&size, pData);
-	bool foundNonZero = false;
-
-	for (size_t idx = 0; idx < mapped.DepthPitch; ++idx)
+	if (FAILED(result))
 	{
-		BYTE b = ((BYTE*)mapped.pData)[idx];
-		(*pData)[idx] = b;
+		printf("Failed to lock created bitmap %x\n", result);
+		return false;
 	}
 
-	delete pData;
+	UINT bmpSize;
+	WICInProcPointer data;
+
+	result = pLock->GetDataPointer(&bmpSize, &data);
+	UINT bmpStride = 0u;
+	if(FAILED(result))
+	{
+		printf("Failed to get Data Pointer %x\n", result);
+		return false;
+	}
+
+	bool foundNonZero = false;
+	
+	pLock->GetStride(&bmpStride);
+
+	printf("Copying information from mapped texture to frame encoder\n");
+	printf("Size: %u Depth Pitch: %u\n", bmpSize, mapped.DepthPitch );
+	printf("BMPStride: %u Row Pitch: %u\n", bmpStride, mapped.RowPitch);
+	printf("Height: %u Depth/Row Pitch: %u\n", stagingDesc.Height, mapped.DepthPitch / mapped.RowPitch);
+	for (size_t rowIdx = 0; rowIdx < stagingDesc.Height; ++rowIdx)
+	{
+		for (size_t colIdx = 0; colIdx < stagingDesc.Width; ++colIdx)
+		{
+			size_t mappedIdx = rowIdx * mapped.RowPitch + colIdx;
+			BYTE b = ((BYTE*)mapped.pData)[mappedIdx];
+
+			size_t bmpIdx = rowIdx * bmpStride + colIdx;
+			if (bmpIdx >= bmpSize)
+			{
+				printf("OVerwriting mapped buffer\n");
+			}
+			else
+			{
+				data[bmpIdx] = b;
+			}
+		}
+	}
+
+	printf("Writing done, outputing to png\n");
 
 	// Done writing
 	pLock->Release();
@@ -330,26 +378,91 @@ bool DissolveMapBaker::RunOnFolder(string folderName, string outputFile)
 	// BoilerPlate to save out
 	IWICBitmapEncoder* pEncoder;
 	GUID containerFormat = GUID_ContainerFormatPng;
-	pFactory->CreateEncoder(containerFormat, nullptr, &pEncoder);
+	result = pFactory->CreateEncoder(containerFormat, nullptr, &pEncoder);
+	if (FAILED(result))
+	{
+		printf("Failed to Create Encoder %x\n", result);
+		return false;
+	}
 
 	std::wstring outputFileW = std::wstring(outputFile.begin(), outputFile.end());
 
 	IWICStream* pStream;
-	pFactory->CreateStream(&pStream);
-	pStream->InitializeFromFilename(outputFileW.c_str(), GENERIC_WRITE);
-	pEncoder->Initialize(pStream, WICBitmapEncoderNoCache);
+	result = pFactory->CreateStream(&pStream);
+
+	if (FAILED(result))
+	{
+		printf("Failed to Create Stream %x\n", result);
+	}
+
+	result = pStream->InitializeFromFilename(outputFileW.c_str(), GENERIC_WRITE);
+
+	if (FAILED(result))
+	{
+		printf("Failed to Initialize from filename %s %x\n", outputFile.c_str(), result);
+		return false;
+	}
+	result = pEncoder->Initialize(pStream, WICBitmapEncoderNoCache);
+
+	if (FAILED(result))
+	{
+		printf("Failed to Initialize from stream %x\n", result);
+		return false;
+	}
 
 	IWICBitmapFrameEncode* pFrameEncode;
 	WICPixelFormatGUID pixelFormat = GUID_WICPixelFormat8bppGray;
-	pEncoder->CreateNewFrame(&pFrameEncode, nullptr);
-	pFrameEncode->Initialize(nullptr);
-	pFrameEncode->SetSize(stagingDesc.Width, stagingDesc.Height);
-	pFrameEncode->SetPixelFormat(&pixelFormat);
 
-	pFrameEncode->WriteSource(pToPng, nullptr);
-	pFrameEncode->Commit();
-	pEncoder->Commit();
+	result = pEncoder->CreateNewFrame(&pFrameEncode, nullptr);
+	if (FAILED(result))
+	{
+		printf("Failed to create new frame %x\n", result);
+		return false;
+	}
 
+	result = pFrameEncode->Initialize(nullptr);
+	if (FAILED(result))
+	{
+		printf("Failed to Initialize frame encoder %x\n", result);
+		return false;
+	}
+
+	result = pFrameEncode->SetSize(stagingDesc.Width, stagingDesc.Height);
+	if (FAILED(result))
+	{
+		printf("Failed to Set Size %d, %d on Frame Encoder %x\n", stagingDesc.Width, stagingDesc.Height, result);
+		return false;
+	}
+
+	result = pFrameEncode->SetPixelFormat(&pixelFormat);
+	if (FAILED(result))
+	{
+		printf("Failed to Set Pixel Format on Frame Encode %x\n", result);
+		return false;
+	}
+
+	result = pFrameEncode->WriteSource(pToPng, nullptr);
+	if (FAILED(result))
+	{
+		printf("Failed Write Source %x\n", result);
+		return false;
+	}
+
+	result = pFrameEncode->Commit();
+	if(FAILED(result))
+	{
+		printf("Failed Commit Frame Encoder %x\n", result);
+		return false;
+	}
+
+	result = pEncoder->Commit();
+	if(FAILED(result))
+	{
+		printf("Failed Commit Encoder %x\n", result);
+		return false;
+	}
+
+	printf("Cleaning up");
 	// Cleanup
 	pFrameEncode->Release();
 	pEncoder->Release();
